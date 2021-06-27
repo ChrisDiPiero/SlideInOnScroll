8c4dac01f7b0d57e6c5694663b105345f4e33300
Chris DiPiero <frogmaster99@msn.com>
2021-06-27 11:54:45 -0500

    completed scroll in events

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,34 @@
-function debounce(func, wait = 20, immediate = true) {
-  var timeout;
+function debounce(func, wait = 20, immediate = true) { //sets timeout to run function only occasionally cousin to THROTTLE
+  let timeout;
   return function() {
-    var context = this, args = arguments;
-    var later = function() {
+    let context = this, args = arguments;
+    const later = function() {
       timeout = null;
       if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
+    }
+    let callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
-  };
+  }
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(slideImage => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+    // bottom of the image
+    const imageBottom = slideImage.offsetTop + slideImage.height;
+    const isHalfShown = slideInAt > slideImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if( isHalfShown && isNotScrolledPast) {
+      slideImage.classList.add('active');
+    } else {
+      slideImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
